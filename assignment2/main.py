f81bf418cd14d72dc3d492d6f30e1f222fce5152
Michael Lam <lamm@eecs.oregonstate.edu>
2016-01-26 21:55:17 -0800

    Forward pass more or less complete

--- assignment2/main.py
+++ assignment2/main.py
@@ -2,28 +2,25 @@ import numpy as np
 from layers.relu import ReluLayer
 from layers.sigmoid import SigmoidLayer
 from layers.linear import LinearLayer
-
-from scipy.special import expit
+from network.sequential import Sequential
 
 # hyperparameters
 num_hidden_units = 10
 learning_rate = 0.01
 mini_batch_size = 256
 momentum = 0.1
+num_epoch = 25
 
 # variables
 input_dim = 10
 
 # network
-linear1 = LinearLayer((num_hidden_units, input_dim))
-relu = ReluLayer()
-linear2 = LinearLayer((num_hidden_units,))
-sigmoid = SigmoidLayer()
+net = Sequential()
+net.add( LinearLayer((num_hidden_units, input_dim)) )
+net.add( ReluLayer() )
+net.add( LinearLayer((num_hidden_units,)) )
+net.add( SigmoidLayer() )
 
 # test
 x = np.random.rand(input_dim)
-
-s1 = linear1.forward(x)
-z1 = relu.forward(s1)
-s2 = linear2.forward(z1)
-z2 = sigmoid.forward(s2)
+z = net.forward(x)
