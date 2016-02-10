a8036e4f7a99795b27a5a2173dcfe2de0efe9404
Michael Lam <lamm@eecs.oregonstate.edu>
2016-02-09 17:07:40 -0800

    Add debug helpers

--- assignment2/network/sequential.py
+++ assignment2/network/sequential.py
@@ -34,17 +34,21 @@ class Sequential:
 	def forward(self, x):
 		self.debug.disp("[forward] Running forward pass...\n")
 		self.debug.disp("[forward] Initial Input={0}\n".format(x))
+		self.debug.disp("[forward] Initial Input Shape={0}\n".format(x.shape))
 
 		z = x
 		for index, layer in enumerate(self.graph):
 			self.debug.disp("[forward] [{0}] {1}".format(index, layer))
 			self.debug.disp("[forward] Input=\n\t\t{0}".format(z))
+			self.debug.disp("[forward] Input Shape=\n\t\t{0}".format(z.shape))
 
 			z = layer.forward(z)
 
 			self.debug.disp("[forward] Output=\n\t\t{0}\n".format(z))
+			self.debug.disp("[forward] Output Shape=\n\t\t{0}\n".format(z.shape))
 
 		self.debug.disp("[forward] Final Output={0}\n".format(z))
+		self.debug.disp("[forward] Final Output Shape={0}\n".format(z.shape))
 		self.debug.disp("[forward] Done with forward pass.")
 
 		return z
@@ -52,18 +56,23 @@ class Sequential:
 	def backward(self, x, grad):
 		self.debug.disp("[backward] Running backward pass...\n")
 		self.debug.disp("[backward] Initial Input x={0}\n".format(x))
+		self.debug.disp("[backward] Initial Input x shape={0}\n".format(x.shape))
 		self.debug.disp("[backward] Initial Input grad={0}\n".format(grad))
+		self.debug.disp("[backward] Initial Input grad shape={0}\n".format(grad.shape))
 
 		g = grad
 		for index, layer in enumerate(reversed(self.graph)):
 			self.debug.disp("[backward] [{0}] {1}".format(index, layer))
 			self.debug.disp("[backward] Input=\n\t\t{0}".format(g))
+			self.debug.disp("[backward] Input Shape=\n\t\t{0}".format(g.shape))
 
 			g = layer.backward(g)
 
 			self.debug.disp("[backward] Output=\n\t\t{0}\n".format(g))
+			self.debug.disp("[backward] Output Shape=\n\t\t{0}\n".format(g.shape))
 
 		self.debug.disp("[backward] Final Output={0}\n".format(g))
+		self.debug.disp("[backward] Final Output Shape={0}\n".format(g.shape))
 		self.debug.disp("[backward] Done with backward pass.")
 
 		return g
